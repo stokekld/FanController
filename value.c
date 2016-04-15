5b07352b037563b2a40f9c2c5368a57811dabf25
Carlos Acosta <arca1983@live.com.mx>
2016-04-15 04:36:04 +0000

    Se aplico la licencia GPL al software

--- value.c
+++ value.c
@@ -1,4 +1,4 @@
-    <Fan Crontoller, control a fan based on the current temperature>
+   /*<Fan Crontoller, control a fan based on the current temperature>
     Copyright (C) <2016>  <Carlos Acosta,Jesus Flores, Rodrigo Galeano,Roberto Guerrero >
 
     This program is free software: you can redistribute it and/or modify
@@ -12,7 +12,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
 #include <stdio.h>
 
