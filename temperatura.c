6ed47bdd91410e4696cbd040ce78b7d2f063876d
Jesus Flores <joy_warmgun@hotmail.com>
2016-04-15 13:04:28 -0500

    Comentando el código 	modified:   Makefile 	modified:   direccion.c 	modified:   insertar.c 	modified:   reservacion.c 	modified:   reservacion.h 	modified:   temperatura.c 	modified:   value.c

--- temperatura.c
+++ temperatura.c
@@ -16,9 +16,11 @@ int leeTemp()
 		return -1;		
 	}
 
+	// obteniendo digitos del archivo
 	temp[0] = fgetc(fd);
 	temp[1] = fgetc(fd);
 
+	// convirtiendo string a entero
 	real = (temp[0] - '0') * 10 + (temp[1] - '0');
 	fclose(fd);
 	return real;
